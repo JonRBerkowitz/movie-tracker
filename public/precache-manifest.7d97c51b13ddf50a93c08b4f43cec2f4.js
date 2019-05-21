4004c8a06dc9650ebbe545ce8e25bf6099136443
jonrberkowitz <jonrberkowitz@gmail.com>
2019-05-21 19:59:37 -0400

    update production env for deploy

---
+++ public/precache-manifest.7d97c51b13ddf50a93c08b4f43cec2f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c553b14019c151cd02a12de6de72e85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be1a9de391c4c1990c08",
+    "url": "/static/css/main.838103ae.chunk.css"
+  },
+  {
+    "revision": "3d2af00204b67f454e17",
+    "url": "/static/js/2.0fbcddc6.chunk.js"
+  },
+  {
+    "revision": "be1a9de391c4c1990c08",
+    "url": "/static/js/main.50cdca09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
